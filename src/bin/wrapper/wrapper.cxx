4a03ec95106e61a1dc4da697b97144cf1e99132a
Karl Semich <0xloem@gmail.com>
2019-04-04 22:13:00 -0400

    wip basic wrapper, symbol issues atm

---
+++ src/bin/wrapper/wrapper.cxx
@@ -0,0 +1,30 @@
+#include <Krypto.ninja.h>
+
+namespace \u20BF {
+
+  class GwExchangeWrapper : public GwExchange {
+    public:
+/**/  virtual bool ready(uS::Loop *const) = 0;                               // wait for exchange and register data handlers
+/**/  virtual void replace(RandId, string) {};                               // call         async orders data from exchange
+/**/  virtual void place(RandId, Side, string, string, OrderType, TimeInForce, bool) = 0, // async orders like above / below
+/**/               cancel(RandId, RandId) = 0,                               // call         async orders data from exchange
+/**/               close() = 0;                                              // disconnect but without waiting for reconnect
+/**/protected:
+/**/  virtual bool            async_wallet() { return false; };              // call         async wallet data from exchange
+/**/  virtual vector<mWallets> sync_wallet() { return {}; };                 // call and read sync wallet data from exchange
+/**/  virtual vector<mLevels>  sync_levels() { return {}; };                 // call and read sync levels data from exchange
+/**/  virtual vector<mTrade>   sync_trades() { return {}; };                 // call and read sync trades data from exchange
+/**/  virtual vector<mOrder>   sync_orders() { return {}; };                 // call and read sync orders data from exchange
+/**/  virtual vector<mOrder>   sync_cancelAll() = 0;                         // call and read sync orders data from exchange
+  };
+
+}
+
+using namespace \u20BF;
+
+extern "C" { Gw * new_Gw__wrapped(const std::string & exchange); }
+
+Gw* \u20BF::Gw::new_Gw(const string& exchange)
+{
+    return new_Gw__wrapped(exchange);
+}
