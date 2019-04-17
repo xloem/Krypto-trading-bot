2203c63887f243171972cd65b06c0a9cd282a8fb
Karl Semich <0xloem@gmail.com>
2019-04-17 11:13:33 -0400

    resolved symbol errors

--- src/bin/wrapper/wrapper.cxx
+++ src/bin/wrapper/wrapper.cxx
@@ -1,3 +1,7 @@
+// include header files, but exclude those that would doubly define symbols
+#define K_BOTS_H_
+#define K_DATA_H_
+#define K_UNITS_H_
 #include <Krypto.ninja.h>
 
 namespace \u20BF {
